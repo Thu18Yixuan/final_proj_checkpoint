8453f794726d928c885907ebc8739db45c39398d
Thu18Yixuan <jiayx18@gmail.com>
2021-04-20 10:13:06 +0800

    Modifying python file

---
+++ fusion_func.py
@@ -0,0 +1,34 @@
+import time
+import json
+import requests
+
+
+def load_cache():
+    try:
+        cache_file = open('cache_final_proj.json', 'r')
+        cache_file_contents = cache_file.read()
+        cache = json.loads(cache_file_contents)
+        cache_file.close()
+    except:
+        cache = {}
+    return cache
+
+
+def save_cache(cache):
+    cache_file = open('cache_final_proj.json', 'w')
+    contents_to_write = json.dumps(cache)
+    cache_file.write(contents_to_write)
+    cache_file.close()
+
+
+def make_url_request_using_cache(url, cache):
+    if (url in cache.keys()):
+        print("Using cache")
+        return cache[url]
+    else:
+        print("Fetching")
+        time.sleep(1)
+        response = requests.get(url)
+        cache[url] = response.text
+        save_cache(cache)
+        return cache[url]
--- test_fusion_api.py
+++ test_fusion_api.py
@@ -3,10 +3,12 @@ import json
 
 API_KEY = "6CDPBSQ9LNshy9YK-_QJ0ePT28fnyUjCzoGdRjBzroNGeB9FlE85vmfBnFt5U2Xrd43cHbrbj2DLYng-CfpT7jeFllLeoJLj_4YkqMUtzQ-g5oNxngIRBFfI0iFmYHYx"
 
-# I do this as the instructions on the webpage but I received a VALIDATION_ERROR, PLEASE HELP!
-url = 'https://api.yelp.com/v3/businesses/north-india-restaurant-san-francisco'
+
+test_url_1 = 'https://api.yelp.com/v3/businesses/north-india-restaurant-san-francisco'
+test_url_2 = 'https://api.yelp.com/v3/businesses/search?term=restaurants&location=New_York'
 headers = {'Authorization': 'Bearer 6CDPBSQ9LNshy9YK-_QJ0ePT28fnyUjCzoGdRjBzroNGeB9FlE85vmfBnFt5U2Xrd43cHbrbj2DLYng-CfpT7jeFllLeoJLj_4YkqMUtzQ-g5oNxngIRBFfI0iFmYHYx'}
-response = requests.get(url, headers=headers)
+response = requests.get(test_url_2, headers=headers)
 json_response = response.json()
 # json_res = json.loads(json_response)
 print (json_response)
+
